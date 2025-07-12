ebde7debc323a06053c2a5a1d50086914cd3d6a6
alxndr29 <alexevan2910@gmail.com>
2025-07-13 00:22:26 +0700

    push

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,28 @@
+import { IsString, IsNotEmpty, IsEmail, MinLength, Matches, IsEnum } from 'class-validator';
+import { Role } from '../enum/role.enum';
+
+export class RegisterDto{
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @MinLength(8)
+  @IsNotEmpty()
+  @Matches(/[A-Z]/,{
+    message: "Password minimal ada huruf kapital"
+  })
+  @Matches(/[0-9]/,{
+    message: "Password minimal ada 1 angka"
+  })
+  @Matches(/[0-9]/,{
+    message: "Password minimal ada 1 karater spesial"
+  })
+  password: string;
+
+  @IsEnum(Role)
+  role: Role;
+}
