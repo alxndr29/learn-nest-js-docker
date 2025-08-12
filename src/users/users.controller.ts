83ef54c3292c7bcff9867efa2203207863aec47a
alxndr29 <alexevan2910@gmail.com>
2025-08-12 23:22:39 +0700

    feat: update user role

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Param, Patch, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from '../auth/entities/user.entity';
 
@@ -6,15 +6,33 @@ import { RolesGuard } from '../auth/guard/roles.guard';
 import { AuthGuard } from '../auth/guard/auth.guard';
 import { Roles } from '../auth/decolators/roles.decolator';
 import { Role } from '../auth/enum/role.enum';
+import { FindOneParams } from './dto/find-one.params';
+import { UpdateRoleDto } from './dto/update-role.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly userService: UsersService){}
+  constructor(private readonly userService: UsersService) {}
 
   @UseGuards(AuthGuard, RolesGuard)
   @Roles(Role.ADMIN)
   @Get()
   async findAllUser(): Promise<User[]> {
-    return await this.userService.findAllUser()
+    return await this.userService.findAllUser();
+  }
+
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
+  @Patch('/:id')
+  async update(@Param() params: FindOneParams, @Body() updateRoleDto: UpdateRoleDto): Promise<User> {
+    const user = await this.findOneById(params.id);
+    return await this.userService.updateRoleUser(user, updateRoleDto);
+  }
+
+  private async findOneById(id: string): Promise<User | null> {
+    const user = await this.userService.findByParams(id);
+    if (!user) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+    return user;
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,15 +2,28 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../auth/entities/user.entity';
 import { Repository } from 'typeorm';
+import { UpdateRoleDto } from './dto/update-role.dto';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private UserRepository: Repository<User>,
-  ){}
+  ) {}
 
-  async findAllUser(): Promise<User[]>{
+  async findAllUser(): Promise<User[]> {
     return await this.UserRepository.find();
+  }
+
+  async findByParams(id: string): Promise<User | null> {
+    return await this.UserRepository.findOneBy({ id });
+  }
+
+  async updateRoleUser(
+    user: User | null,
+    updateRoleDto: UpdateRoleDto,
+  ): Promise<User> {
+    Object.assign(user, updateRoleDto);
+    return await this.UserRepository.save(user);
   }
 }
