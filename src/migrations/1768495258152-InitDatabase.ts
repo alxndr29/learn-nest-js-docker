d1e519df5acc6bedaf090ebcd2cd7f6ad745a93e
Alexander Evan <61173884+alxndr29@users.noreply.github.com>
2026-01-15 23:41:45 +0700

    Create 1768495258152-InitDatabase.ts

---
+++ src/migrations/1768495258152-InitDatabase.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InitDatabase1768495258152 implements MigrationInterface {
+    name = 'InitDatabase1768495258152'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "article_tag" DROP CONSTRAINT "FK_602d4921b27c9a7cb6c095992b4"`);
+        await queryRunner.query(`ALTER TABLE "article_tag" DROP CONSTRAINT "FK_bbbd0832bdd107597b596d63f69"`);
+        await queryRunner.query(`ALTER TABLE "article_tag" ADD CONSTRAINT "FK_bbbd0832bdd107597b596d63f69" FOREIGN KEY ("tagId") REFERENCES "tag"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "article_tag" ADD CONSTRAINT "FK_602d4921b27c9a7cb6c095992b4" FOREIGN KEY ("articleId") REFERENCES "article"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "article_tag" DROP CONSTRAINT "FK_602d4921b27c9a7cb6c095992b4"`);
+        await queryRunner.query(`ALTER TABLE "article_tag" DROP CONSTRAINT "FK_bbbd0832bdd107597b596d63f69"`);
+        await queryRunner.query(`ALTER TABLE "article_tag" ADD CONSTRAINT "FK_bbbd0832bdd107597b596d63f69" FOREIGN KEY ("tagId") REFERENCES "tag"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "article_tag" ADD CONSTRAINT "FK_602d4921b27c9a7cb6c095992b4" FOREIGN KEY ("articleId") REFERENCES "article"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+}
