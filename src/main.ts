253cd986f234ec03a5446f42a208d26a8faeb3a9
alxndr29 <alexevan2910@gmail.com>
2026-02-05 15:22:47 +0700

    open cors origin

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,13 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+  // app.enableCors({
+  //   origin: 'http://localhost:5173',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   credentials: true,
+  // });
   app.enableCors({
-    origin: 'http://localhost:5173',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: true,
     credentials: true,
   });
   const config = new DocumentBuilder()
